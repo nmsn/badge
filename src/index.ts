45453584cce3fda4ad757e65bd6841f34cd28718
nmsn <136696700@qq.com>
2022-12-14 14:15:17 +0800

    feat: add ui

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import type { BaseBadgeType } from "./constant";
+import { baseBadges } from "./constant";
 import { makeBadge } from "./utils";
 
-makeBadge(null, "BADGE");
+const prompts = [
+  {
+    type: "checkbox",
+    name: "badges",
+    message: "Select the option what you want to generate a badge?",
+    pageSize: 10,
+    choices: baseBadges.map((item) => ({
+      name: `${item.title} ${chalk
+        .hex(item.backgroundColor)
+        .inverse(" C ")}`,
+      value: item.title,
+    })),
+  },
+  {
+    type: "input",
+    name: "fileName",
+    message: "Enter a name for the save file",
+    default: "BADGE",
+  },
+];
+
+const filterSelectedBadge = (
+  badges: BaseBadgeType[],
+  badgeTitles: string[]
+) => {
+  return badges.filter((item) => badgeTitles.includes(item.title));
+};
+
+export default async () => {
+  const { badges, fileName } = await inquirer.prompt(prompts);
+
+  const selectedBadges = filterSelectedBadge(baseBadges, badges);
+
+  makeBadge(selectedBadges, fileName);
+};
