62548b5a9423e05b91b4b40b03d4db4cf5ae11b8
nmsn <136696700@qq.com>
2022-12-14 09:42:58 +0800

    feat: add badge constant

---
+++ src/constant.ts
@@ -0,0 +1,71 @@
+/** https://simpleicons.org/ */
+export const baseBadge = [
+  {
+    title: "JavaScript",
+    backgroundColor: "#F7DF1E",
+  },
+  {
+    title: "TypeScript",
+    backgroundColor: "#3178C6",
+  },
+  {
+    title: "React",
+    backgroundColor: "#61DAFB",
+  },
+  {
+    title: "Vue",
+    backgroundColor: "#4FC08D",
+  },
+  {
+    title: "Next.js",
+    backgroundColor: "#000000",
+  },
+  {
+    title: "Node.js",
+    backgroundColor: "#339933",
+  },
+  {
+    title: "Express",
+    backgroundColor: "#000000",
+  },
+  {
+    title: "Koa",
+    backgroundColor: "#33333D",
+  },
+  {
+    title: "Less",
+    backgroundColor: "#1D365D",
+  },
+  {
+    title: "Sass",
+    backgroundColor: "#CC6699",
+  },
+  {
+    title: "CSS Modules",
+    backgroundColor: "#000000",
+  },
+  {
+    title: "PostCSS",
+    backgroundColor: "#DD3A0A",
+  },
+  {
+    title: "Sass",
+    backgroundColor: "#CC6699",
+  },
+  {
+    title: "Tailwind CSS",
+    backgroundColor: "#06B6D4",
+  },
+  {
+    title: "Testing Library",
+    backgroundColor: "#E33332",
+  },
+  {
+    title: "Jest",
+    backgroundColor: "#C21325",
+  },
+  {
+    title: "Markdown",
+    backgroundColor: "#000000",
+  },
+];
